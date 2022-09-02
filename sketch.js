6039fc1df22e11fa14010af6ecc5e78229be2cd9
Fabricio Dos Santos <fabricioperazzo@gmail.com>
2022-09-02 13:14:09 -0400

    Created 16x16 grid of divs, displayed as squares in the webpage

--- sketch.js
+++ sketch.js
@@ -1,16 +1,30 @@
+const body = document.querySelector("body");
+
 const container = document.createElement("div");
 container.classList.add("container");
+
+body.appendChild(container);
 
 for (let i = 0; i < 16; i++){
     let rowDiv = document.createElement("div");
     rowDiv.classList.add("row");
+    rowDiv.id = `row${i}`;
     container.appendChild(rowDiv);
 }
 
-const gridOfDivs = [];
-
+let j = 0;
 for (let i = 0; i < 256; i++){
+    if (i !== 0 && i % 16 === 0){
+        j++;
+    }
+
+    let rowDivToAdd = document.querySelector(`#row${j}`);
+
     let squareDiv = document.createElement("div");
     squareDiv.classList.add("square");
-    gridOfDivs[i] = squareDiv;
+
+    rowDivToAdd.appendChild(squareDiv);
 }
+
+
+
