0800ded2bb93de4a5f1aa2f7c5bda16f67ec0a1b
Fabricio Dos Santos <fabricioperazzo@gmail.com>
2022-09-02 14:26:05 -0400

    Added size and erase buttons to webpage and stylised them

--- sketch.js
+++ sketch.js
@@ -30,7 +30,7 @@ for (let i = 0; i < 256; i++){
 
 // Adding hovering effect to each square div
 
-const squaresArray = document.querySelectorAll(".square");
+let squaresArray = document.querySelectorAll(".square");
 
 squaresArray.forEach((square) => {
     square.addEventListener("mouseover", (event) => {
@@ -38,3 +38,48 @@ squaresArray.forEach((square) => {
     })
 })
 
+// Adding size and erase buttons to display
+
+const buttonsDisplay = document.createElement("div");
+buttonsDisplay.classList.add("buttonsDisplay");
+container.appendChild(buttonsDisplay);
+
+const sizeButtons = document.createElement("div");
+sizeButtons.classList.add("sizeButtons");
+
+const textDiv = document.createElement("div");
+textDiv.textContent = "GRID SIZE:";
+sizeButtons.appendChild(textDiv);
+
+const smallButton = document.createElement("button");
+const mediumButton = document.createElement("button");
+const bigButton = document.createElement("button");
+
+smallButton.classList.add("buttons");
+mediumButton.classList.add("buttons");
+bigButton.classList.add("buttons");
+
+smallButton.textContent = "Small";
+mediumButton.textContent = "Medium";
+bigButton.textContent = "Big";
+
+sizeButtons.appendChild(smallButton);
+sizeButtons.appendChild(mediumButton);
+sizeButtons.appendChild(bigButton);
+
+const eraseButtonDisplay = document.createElement("div");
+eraseButtonDisplay.classList.add("eraseButtonDisplay");
+
+const eraseButton = document.createElement("button");
+eraseButton.classList.add("buttons");
+eraseButton.textContent = "Erase";
+eraseButtonDisplay.appendChild(eraseButton);
+
+buttonsDisplay.appendChild(sizeButtons);
+buttonsDisplay.appendChild(eraseButtonDisplay);
+
+// Adding functionality to the buttons
+
+eraseButton.addEventListener("click", (event) => {
+
+})
